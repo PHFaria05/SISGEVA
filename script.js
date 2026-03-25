cfbe33a803daeb907ef1cfdd0bb223b54ca57ea8
PHFaria05 <p.h.faria@uni9.edu.br>
2026-03-25 18:52:27 -0300

    correção do modal e adição de salvamento de paciente

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ function login() {
   const pass = document.getElementById("pass").value;
 
   if (user === "admin" && pass === "123") {
-    document.getElementById("loginScreen").style.display = "none";
-    document.getElementById("mainScreen").style.display = "block";
+    document.getElementById("loginScreen").classList.add("hidden");
+    document.getElementById("mainScreen").classList.remove("hidden");
   } else {
     alert("Usuário ou senha inválidos");
   }
+}
+
+function abrirModal() {
+  document.getElementById("modalPaciente").classList.add("active");
+}
+
+function fecharModal() {
+  document.getElementById("modalPaciente").classList.remove("active");
+}
+
+let pacientes = [];
+
+function salvarPaciente() {
+  const nome = document.getElementById("nomePaciente").value;
+
+  if (nome === "") {
+    alert("Digite um nome");
+    return;
+  }
+
+  pacientes.push(nome);
+
+  console.log(pacientes);
+
+  alert("Paciente salvo!");
+
+  document.getElementById("nomePaciente").value = "";
 }
